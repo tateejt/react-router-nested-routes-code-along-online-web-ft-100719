9013f1c333f612c97f4a675859da9a0bc75bc414
Emery Tate <tateejt@gmail.com>
2020-03-06 05:00:42 +0000

    Automatically backed up by Learn

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+mport React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from '../components/MovieShow';
@@ -6,7 +6,7 @@ import MovieShow from '../components/MovieShow';
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-    <Route path={`${match.url}/:movieId`} component={<MovieShow movies={movies} /> }/>
+    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
 )
  
